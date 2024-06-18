cf9ae117fdb4b662e8c1f7187f7f85b568225eaf
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-18 11:15:15 +0200

    Los ending funcionan

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ app.get('/suma/:sumando1&:sumando2', (req, res) => {
     var suma1 = Number(req.params.sumando1);
     var suma2 = Number(req.params.sumando2);
     resultado = suma1 + suma2;
-    res.send('resultado')
+    res.send(resultado)
 })
 
 app.get('/multiplicacion/:multiplicando1&:multiplicando2', (req, res) => {
     var mult1 = Number(req.params.multiplicando1);
     var mult2 = Number(req.params.multiplicando2);
     resultado = mult1 * mult2;
-    res.send('resultado')
+    res.send(resultado)
 })
 
 app.get('/lista-de-1/:a', (req, res) => {
@@ -23,7 +23,7 @@ app.get('/lista-de-1/:a', (req, res) => {
 
         resultado = lista.push(i)
     }
-    res.send('resultado')
+    res.send(resultado)
 
 })
 

50a95526a37fe41533146132d77883b8a9ba587d
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-18 11:11:06 +0200

    Creada función de lista con límite de :a

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
-
 app.get('/suma/:sumando1&:sumando2', (req, res) => {
     var suma1 = Number(req.params.sumando1);
     var suma2 = Number(req.params.sumando2);
@@ -18,7 +16,16 @@ app.get('/multiplicacion/:multiplicando1&:multiplicando2', (req, res) => {
     res.send('resultado')
 })
 
+app.get('/lista-de-1/:a', (req, res) => {
+    var a = Number(req.params.a);
+    for (i = 0; i <= a; i++) {
+        var lista = [];
 
+        resultado = lista.push(i)
+    }
+    res.send('resultado')
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)

22eb25f72a6d303be83b2a62dbcbb4abfeb0abc1
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-18 10:42:36 +0200

    Creado endpoint /suma

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/hola', (req, res) => {
-  res.send('Hello World!')
+
+
+app.get('/suma/:sumando1/:sumando2', (req, res) => {
+  var suma1 = Number(req.params.sumando1);
+  var suma2 = Number(req.params.sumando2);
+  resultado = suma1 + suma2;
+      res.send("resultado")
 })
 
 app.listen(port, () => {
